92cfe48e387d7562d2c66f13b06bcfc49d256ee3
HirobumiNakamura <nakamura@step-info.co.jp>
2025-07-21 16:43:08 +0900

    Revert "pythonテストコード作成"

--- pytest_sample/src/main.py
+++
@@ -1,9 +0,0 @@
-from mod import module_1
-
-
-def main(a: int, b: int) -> int:
-    return a + b
-
-
-def hello() -> str:
-    return module_1.hello()
--- pytest_sample/tests/mod/test_module_1.py
+++
@@ -1,4 +0,0 @@
-def test_hello():
-    from mod import module_1
-
-    assert module_1.hello() == "Hello from module_1"
